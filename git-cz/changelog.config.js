77a2289fb5cc3e047cd6eef49e8029b59c81db38
Vitor Delfino <vitor.delfino@loft.com.br>
2022-02-22 18:20:11 -0300

    02/22/2022

---
+++ git-cz/changelog.config.js
@@ -0,0 +1,75 @@
+module.exports= {
+  disableEmoji: false,
+  format: "{type}{scope}: {subject}{emoji}",
+  list: [
+    "test",
+    "feat",
+    "fix",
+    "chore",
+    "docs",
+    "refactor",
+    "style",
+    "ci",
+    "perf",
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: [
+    "type",
+    "scope",
+    "subject",
+  ],
+  scopes: [],
+  types: {
+    chore: {
+      description: "Build process or auxiliary tool changes",
+      emoji: "🔧",
+      value: "chore",
+    },
+    ci: {
+      description: "CI related changes",
+      emoji: "💚",
+      value: "ci",
+    },
+    docs: {
+      description: "Documentation only changes",
+      emoji: "📝",
+      value: "docs",
+    },
+    feat: {
+      description: "A new feature",
+      emoji: "✨",
+      value: "feat",
+    },
+    fix: {
+      description: "A bug fix",
+      emoji: "🐛",
+      value: "fix",
+    },
+    perf: {
+      description: "A code change that improves performance",
+      emoji: "⚡",
+      value: "perf",
+    },
+    refactor: {
+      description: "A code change that neither fixes a bug or adds a feature",
+      emoji: "♻️",
+      value: "refactor",
+    },
+    release: {
+      description: "Create a release commit",
+      emoji: "🏹",
+      value: "release",
+    },
+    style: {
+      description: "Markup, white-space, formatting, missing semi-colons...",
+      emoji: "💄",
+      value: "style",
+    },
+    test: {
+      description: "Adding missing tests",
+      emoji: "✅",
+      value: "test",
+    },
+  },
+}
